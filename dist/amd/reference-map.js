1e64bbed9d12162aefb9465e19a5974b71837876
Justin Meyer <justinbmeyer@gmail.com>
2016-07-28 15:48:19 -0500

    3.0.0-pre.12

---
+++ dist/amd/reference-map.js
@@ -0,0 +1,14 @@
+/*can-view-scope@3.0.0-pre.11#reference-map*/
+define(function (require, exports, module) {
+    var types = require('can-util/js/types');
+    var SimpleMap = require('can-simple-map');
+    var ReferenceMap = SimpleMap.extend({});
+    var oldIsMapLike = types.isMapLike;
+    types.isMapLike = function (obj) {
+        if (obj instanceof ReferenceMap) {
+            return true;
+        }
+        return oldIsMapLike.call(this, obj);
+    };
+    module.exports = ReferenceMap;
+});
