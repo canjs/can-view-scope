c580271cb517e193d10da4f601acef39dbb0b7e8
Justin Meyer <justinbmeyer@gmail.com>
2017-02-17 16:44:39 -0600

    3.1.3

---
+++ dist/amd/compute_data.js
@@ -0,0 +1,81 @@
+/*can-view-scope@3.1.2#compute_data*/
+define(function (require, exports, module) {
+    var Observation = require('can-observation');
+    var observeReader = require('can-observation/reader');
+    var makeCompute = require('can-compute');
+    var types = require('can-types');
+    var isFunction = require('can-util/js/is-function');
+    var isEmptyObject = require('can-util/js/is-empty-object');
+    var isFastPath = function (computeData) {
+        if (computeData.reads && computeData.reads.length === 1) {
+            var root = computeData.root;
+            if (types.isCompute(root)) {
+                root = root();
+            }
+            return types.isMapLike(root) && !isFunction(root[computeData.reads[0].key]);
+        }
+        return;
+    };
+    var scopeReader = function (scope, key, options, computeData, newVal) {
+        if (arguments.length > 4) {
+            var root = computeData.root || computeData.setRoot;
+            if (root) {
+                observeReader.write(root, computeData.reads, newVal, options);
+            } else {
+                scope.set(key, newVal, options);
+            }
+        } else {
+            if (computeData.root) {
+                return observeReader.read(computeData.root, computeData.reads, options).value;
+            }
+            var data = scope.read(key, options);
+            computeData.scope = data.scope;
+            computeData.initialValue = data.value;
+            computeData.reads = data.reads;
+            computeData.root = data.rootObserve;
+            computeData.setRoot = data.setRoot;
+            return data.value;
+        }
+    };
+    module.exports = function (scope, key, options) {
+        options = options || { args: [] };
+        var computeData = {}, scopeRead = function (newVal) {
+                if (arguments.length) {
+                    return scopeReader(scope, key, options, computeData, newVal);
+                } else {
+                    return scopeReader(scope, key, options, computeData);
+                }
+            }, compute = makeCompute(undefined, {
+                on: function () {
+                    observation.start();
+                    if (isFastPath(computeData)) {
+                        observation.dependencyChange = function (ev, newVal) {
+                            if (types.isMapLike(ev.target) && typeof newVal !== 'function') {
+                                this.newVal = newVal;
+                            } else {
+                                observation.dependencyChange = Observation.prototype.dependencyChange;
+                                observation.start = Observation.prototype.start;
+                                compute.fastPath = false;
+                            }
+                            return Observation.prototype.dependencyChange.call(this, ev);
+                        };
+                        observation.start = function () {
+                            this.value = this.newVal;
+                        };
+                        compute.fastPath = true;
+                    }
+                    compute.computeInstance.value = observation.value;
+                    compute.computeInstance.hasDependencies = !isEmptyObject(observation.newObserved);
+                },
+                off: function () {
+                    observation.stop();
+                },
+                set: scopeRead,
+                get: scopeRead,
+                __selfUpdater: true
+            }), observation = new Observation(scopeRead, null, compute.computeInstance);
+        compute.computeInstance.observation = observation;
+        computeData.compute = compute;
+        return computeData;
+    };
+});
