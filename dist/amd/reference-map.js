d6ae1d1c6c2932f7e55effeccd1a581e9e5aa072
Kevin Phillips <kevin@bitovi.com>
2017-06-14 15:04:13 -0500

    3.2.0-pre.12

---
+++ dist/amd/reference-map.js
@@ -0,0 +1,14 @@
+/*can-view-scope@3.2.0-pre.11#reference-map*/
+define(function (require, exports, module) {
+    var types = require('can-types');
+    var SimpleMap = require('can-simple-map');
+    var ReferenceMap = SimpleMap.extend({});
+    var oldIsMapLike = types.isMapLike;
+    types.isMapLike = function (obj) {
+        if (obj instanceof ReferenceMap) {
+            return true;
+        }
+        return oldIsMapLike.call(this, obj);
+    };
+    module.exports = ReferenceMap;
+});
