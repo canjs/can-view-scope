84236d91c5b12fe82ad59b574292ccb34c3c9527
Kevin Phillips <kevin@bitovi.com>
2017-06-14 13:35:12 -0500

    3.2.0-pre.11

---
+++ dist/amd/compute_data.js
@@ -0,0 +1,146 @@
+/*can-view-scope@3.2.0-pre.10#compute_data*/
+define(function (require, exports, module) {
+    'use strict';
+    var Observation = require('can-observation');
+    var observeReader = require('can-observation/reader');
+    var makeCompute = require('can-compute');
+    var assign = require('can-util/js/assign');
+    var types = require('can-types');
+    var isFunction = require('can-util/js/is-function');
+    var canBatch = require('can-event/batch');
+    var CID = require('can-cid');
+    var canReflect = require('can-reflect');
+    var canSymbol = require('can-symbol');
+    var getFastPathRoot = function (computeData) {
+        if (computeData.reads && computeData.reads.length === 1) {
+            var root = computeData.root;
+            if (types.isCompute(root)) {
+                root = canReflect.getValue(root);
+            }
+            return types.isMapLike(root) && !isFunction(root[computeData.reads[0].key]) && root;
+        }
+        return;
+    };
+    var isEventObject = function (obj) {
+        return obj && typeof obj.batchNum === 'number' && typeof obj.type === 'string';
+    };
+    var ScopeKeyData = function (scope, key, options) {
+        CID(this);
+        this.startingScope = scope;
+        this.key = key;
+        this.observation = new Observation(this.read, this);
+        this.options = assign({ observation: this.observation }, options);
+        this.handlers = [];
+        this.dispatchHandler = this.dispatch.bind(this);
+        this.fastPath = undefined;
+        this.root = undefined;
+        this.initialValue = undefined;
+        this.reads = undefined;
+        this.setRoot = undefined;
+    };
+    ScopeKeyData.prototype.getValue = function () {
+        Observation.add(this);
+        return this.getObservationValue();
+    };
+    ScopeKeyData.prototype.getObservationValue = Observation.ignore(function () {
+        return this.observation.get();
+    });
+    ScopeKeyData.prototype.read = function () {
+        if (this.root) {
+            return observeReader.read(this.root, this.reads, this.options).value;
+        }
+        var data = this.startingScope.read(this.key, this.options);
+        this.scope = data.scope;
+        this.reads = data.reads;
+        this.root = data.rootObserve;
+        this.setRoot = data.setRoot;
+        return this.initialValue = data.value;
+    };
+    ScopeKeyData.prototype.setValue = function (newVal) {
+        var root = this.root || this.setRoot;
+        if (root) {
+            observeReader.write(root, this.reads, newVal, this.options);
+        } else {
+            this.startingScope.set(this.key, newVal, this.options);
+        }
+    };
+    ScopeKeyData.prototype.hasDependencies = function () {
+        return this.observation.hasDependencies();
+    };
+    var canOnValue = canSymbol.for('can.onValue'), canOffValue = canSymbol.for('can.offValue');
+    canReflect.set(ScopeKeyData.prototype, canOnValue, function (handler) {
+        if (!this.handlers.length) {
+            canReflect.onValue(this.observation, this.dispatchHandler);
+            var fastPathRoot = getFastPathRoot(this);
+            if (fastPathRoot) {
+                var self = this, observation = this.observation;
+                this.fastPath = true;
+                observation.dependencyChange = function (target, newVal, altNewValue) {
+                    if (isEventObject(newVal)) {
+                        newVal = altNewValue;
+                    }
+                    if (target === fastPathRoot && typeof newVal !== 'function') {
+                        this.newVal = newVal;
+                    } else {
+                        observation.dependencyChange = Observation.prototype.dependencyChange;
+                        observation.start = Observation.prototype.start;
+                        self.fastPath = false;
+                    }
+                    return Observation.prototype.dependencyChange.call(this, target, newVal, altNewValue);
+                };
+                observation.start = function () {
+                    this.value = this.newVal;
+                };
+            }
+        }
+        this.handlers.push(handler);
+    });
+    ScopeKeyData.prototype.dispatch = function () {
+        var handlers = this.handlers.slice(0);
+        for (var i = 0, len = handlers.length; i < len; i++) {
+            canBatch.batchNum = this.observation.batchNum;
+            handlers[i].apply(this, arguments);
+        }
+    };
+    canReflect.set(ScopeKeyData.prototype, canOffValue, function (handler) {
+        var index = this.handlers.indexOf(handler);
+        this.handlers.splice(index, 1);
+        if (!this.handlers.length) {
+            canReflect.offValue(this.observation, this.dispatchHandler);
+        }
+    });
+    canReflect.set(ScopeKeyData.prototype, canSymbol.for('can.getValue'), ScopeKeyData.prototype.getValue);
+    canReflect.set(ScopeKeyData.prototype, canSymbol.for('can.setValue'), ScopeKeyData.prototype.setValue);
+    canReflect.set(ScopeKeyData.prototype, canSymbol.for('can.valueHasDependencies'), ScopeKeyData.prototype.hasDependencies);
+    Object.defineProperty(ScopeKeyData.prototype, 'compute', {
+        get: function () {
+            var scopeKeyData = this;
+            var compute = makeCompute(undefined, {
+                on: function (updater) {
+                    scopeKeyData[canOnValue](updater);
+                    this.value = scopeKeyData.observation.value;
+                },
+                off: function (updater) {
+                    scopeKeyData[canOffValue](updater);
+                },
+                get: function () {
+                    return scopeKeyData.observation.get();
+                },
+                set: function (newValue) {
+                    return scopeKeyData.setValue(newValue);
+                }
+            });
+            compute.computeInstance.observation = this.observation;
+            Object.defineProperty(this, 'compute', {
+                value: compute,
+                writable: false,
+                configurable: false
+            });
+            return compute;
+        },
+        configurable: true
+    });
+    module.exports = function (scope, key, options) {
+        return new ScopeKeyData(scope, key, options || { args: [] });
+    };
+});
