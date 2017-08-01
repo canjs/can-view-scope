7b6a69f941c2f06fbf0a7b94a8aa6e153c07c62f
John Gardner <john@ardea.io>
2017-08-01 15:12:51 -0500

    3.3.2

---
+++ dist/amd/reference-map.js
@@ -0,0 +1,11 @@
+/*can-view-scope@3.3.1#reference-map*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-simple-map'
+], function (require, exports, module) {
+    var SimpleMap = require('can-simple-map');
+    var ReferenceMap = SimpleMap.extend({});
+    module.exports = ReferenceMap;
+});
