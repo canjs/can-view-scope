e487df310a428d62eb796018d79035ab5663d8d9
imasustink <austin@austinkurpuis.com>
2017-11-01 15:59:33 -0700

    3.5.3

---
+++ dist/amd/template-context.js
@@ -0,0 +1,78 @@
+/*can-view-scope@3.5.2#template-context*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-symbol',
+    'can-simple-map',
+    'can-define-lazy-value',
+    'can-log/dev',
+    'can-observation'
+], function (require, exports, module) {
+    var canSymbol = require('can-symbol');
+    var SimpleMap = require('can-simple-map');
+    var defineLazyValue = require('can-define-lazy-value');
+    var dev = require('can-log/dev');
+    var observation = require('can-observation');
+    var getKeyValueSymbol = canSymbol.for('can.getKeyValue'), setKeyValueSymbol = canSymbol.for('can.setKeyValue');
+    var nonObservableVars = {
+        index: true,
+        key: true,
+        element: true,
+        event: true,
+        viewModel: true,
+        arguments: true,
+        lineNumber: true,
+        filename: true
+    };
+    var getKeyAndParent = observation.ignore(function (templateContext, key) {
+        var parent = templateContext;
+        if (key.substr(0, 6) === 'scope.') {
+            key = key.substr(6);
+        } else if (key === '*self') {
+            key = 'view';
+        } else if (key.substr(0, 1) === '*') {
+            key = key.substr(1);
+            parent = parent.vars;
+        }
+        if (nonObservableVars[key]) {
+            parent = parent.nonObservableVars;
+        }
+        if (key.substr(0, 5) === 'vars.') {
+            key = key.substr(5);
+            parent = parent.vars;
+        }
+        return {
+            key: key,
+            parent: parent
+        };
+    });
+    var TemplateContext = SimpleMap.extend('TemplateContext', {});
+    defineLazyValue(TemplateContext.prototype, 'vars', function () {
+        return new SimpleMap({});
+    });
+    defineLazyValue(TemplateContext.prototype, 'nonObservableVars', function () {
+        return {};
+    });
+    TemplateContext.prototype[getKeyValueSymbol] = function (originalKey) {
+        var keyAndParent = getKeyAndParent(this, originalKey);
+        var key = keyAndParent.key;
+        var parent = keyAndParent.parent;
+        if (parent instanceof SimpleMap) {
+            return SimpleMap.prototype[getKeyValueSymbol].call(parent, key);
+        } else {
+            return parent[key];
+        }
+    };
+    TemplateContext.prototype[setKeyValueSymbol] = function (originalKey, value) {
+        var keyAndParent = getKeyAndParent(this, originalKey);
+        var key = keyAndParent.key;
+        var parent = keyAndParent.parent || this;
+        if (parent instanceof SimpleMap) {
+            return SimpleMap.prototype[setKeyValueSymbol].call(parent, key, value);
+        } else {
+            return parent[key] = value;
+        }
+    };
+    module.exports = TemplateContext;
+});
