6dd12aaeecf4e34d8c79107fa6e12891689be6c2
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-05-10 18:52:54 +0100

    Landscaper: QUnit2 upgrade

--- test/variable-scope-test.js
+++ test/variable-scope-test.js
@@ -5,7 +5,7 @@ var canReflect = require("can-reflect");
 
 QUnit.module('can-view-scope variable scope');
 
-QUnit.test("reading", function(){
+QUnit.test("reading", function(assert) {
 	var root = {
 		rootProp: "ROOT",
 		conflictProp: "ROOT"
@@ -15,14 +15,14 @@ QUnit.test("reading", function(){
 		conflictProp: "VARIABLE"
 	},{variable: true});
 
-	QUnit.equal( scope.get("variableProp"), "VARIABLE", "can read a variable");
-	QUnit.equal( scope.get("this.rootProp"), "ROOT", "can pass variables for the root");
+	assert.equal( scope.get("variableProp"), "VARIABLE", "can read a variable");
+	assert.equal( scope.get("this.rootProp"), "ROOT", "can pass variables for the root");
 
-	QUnit.equal( scope.get("this.conflictProp"), "ROOT", "this.conflictProp");
-	QUnit.equal( scope.get("./conflictProp"), "ROOT", "./conflictProp");
-	QUnit.equal( scope.get("conflictProp"), "VARIABLE", "conflictProp");
+	assert.equal( scope.get("this.conflictProp"), "ROOT", "this.conflictProp");
+	assert.equal( scope.get("./conflictProp"), "ROOT", "./conflictProp");
+	assert.equal( scope.get("conflictProp"), "VARIABLE", "conflictProp");
 
-	QUnit.equal( scope.get("this"), root, "this is right");
+	assert.equal( scope.get("this"), root, "this is right");
 
 	var root2 = {
 		root2Prop: "ROOT2",
@@ -33,14 +33,14 @@ QUnit.test("reading", function(){
 		conflictProp: "VARIABLE"
 	},{variable: true});
 
-	QUnit.equal( scope2.get("variableProp"), "VARIABLE", "can read a variable");
-	QUnit.equal( scope2.get("this.root2Prop"), "ROOT2", "can pass variables for the root 2");
+	assert.equal( scope2.get("variableProp"), "VARIABLE", "can read a variable");
+	assert.equal( scope2.get("this.root2Prop"), "ROOT2", "can pass variables for the root 2");
 
-	QUnit.equal( scope2.get("this.conflictProp"), "ROOT2", "this.conflictProp");
-	QUnit.equal( scope2.get("./conflictProp"), "ROOT2", "./conflictProp");
-	QUnit.equal( scope2.get("conflictProp"), "VARIABLE", "conflictProp");
+	assert.equal( scope2.get("this.conflictProp"), "ROOT2", "this.conflictProp");
+	assert.equal( scope2.get("./conflictProp"), "ROOT2", "./conflictProp");
+	assert.equal( scope2.get("conflictProp"), "VARIABLE", "conflictProp");
 
-	QUnit.equal( scope2.get("../conflictProp"), "ROOT", "../conflictProp");
+	assert.equal( scope2.get("../conflictProp"), "ROOT", "../conflictProp");
 
 	var root3 = {
 		root3Prop: "ROOT3",
@@ -51,32 +51,32 @@ QUnit.test("reading", function(){
 		conflictProp: "VARIABLE"
 	},{variable: true});
 
-	QUnit.equal( scope3.get("../../conflictProp"), "ROOT", "../../conflictProp");
+	assert.equal( scope3.get("../../conflictProp"), "ROOT", "../../conflictProp");
 });
 
-QUnit.test("writing", function(){
+QUnit.test("writing", function(assert) {
 	var root = new SimpleMap({name: "ROOT"});
 	var scope;
 
 	scope = new Scope(root).addLetContext();
 
 	scope.set("rootProp","VALUE");
-	QUnit.equal(root.get("rootProp"), "VALUE", "wrote to property with .set");
+	assert.equal(root.get("rootProp"), "VALUE", "wrote to property with .set");
 
 	var rootProp = scope.computeData('rootProp2');
 	canReflect.setValue(rootProp, "VALUE2");
-	QUnit.equal(root.get("rootProp2"), "VALUE2", "wrote property by setting ScopeKeyData");
+	assert.equal(root.get("rootProp2"), "VALUE2", "wrote property by setting ScopeKeyData");
 
 	var rootProp3 = scope.computeData('rootProp3');
 	canReflect.onValue(rootProp3, function(){});
 	canReflect.setValue(rootProp3, "VALUE3");
-	QUnit.equal(root.get("rootProp3"), "VALUE3", "wrote to property by setting bound ScopeKeyData");
+	assert.equal(root.get("rootProp3"), "VALUE3", "wrote to property by setting bound ScopeKeyData");
 
 
 	scope = new Scope(root).addLetContext({tempProp: undefined});
 
 	scope.set("tempProp", "foo");
 
-	QUnit.equal(root.get("tempProp"), undefined, "write to undefined not set on root");
-	QUnit.equal(scope.get("tempProp"), "foo", "able to read from root");
+	assert.equal(root.get("tempProp"), undefined, "write to undefined not set on root");
+	assert.equal(scope.get("tempProp"), "foo", "able to read from root");
 });
